8b58ecb8dac03f930264db1c3ae344fc2f162b47
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2016-10-14 14:46:52 -0700

    demo.js

--- demo.js
+++ demo.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ReactImageCrop from "react-image-crop-component";
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactImageCrop = require('react-image-crop-component');
 
 var Demo = React.createClass({
     render(){
         return (<div style={{width: "300px", height: "300px"}}>
-            <ReactImageCrop square={true} onCrop={this.onCropped} onCropData={this.onCroppedData}  onChange={this.onChanged} src="demo.jpg"/>
+            <ReactImageCrop square={false} onCrop={this.onCropped} onCropData={this.onCroppedData}  onChange={this.onChanged} src="demo.jpg"/>
         </div>);
     },
     onChanged: function (e) {
