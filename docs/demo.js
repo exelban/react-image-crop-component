671ae5e162048c34a0c00da1fcdb8d507f24c8f8
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2017-02-11 19:46:52 +0100

    Update

--- docs/demo.js
+++ docs/demo.js
@@ -5,11 +5,18 @@ require ('./../lib/style.css');
 
 const MainView = React.createClass({
     render(){
-        return (
-            <div>
-                <ReactImageCrop square={false} src="./demo.jpg"/>
-            </div>
-        );
+        return (<div style={{width: "700px", height: "500px", margin: "0 auto"}}>
+            <ReactImageCrop square={false} onCrop={this.onCropped} onCropData={this.onCroppedData}  onChange={this.onChanged} src="demo.jpg"/>
+        </div>);
+    },
+    onChanged: function (e) {
+        console.log("Changed");
+    },
+    onCropped: function (e) {
+        console.log("Cropped image", e);
+    },
+    onCroppedData: function (e) {
+        console.log("Cropped size", e);
     }
 });
 
